1d192079fb43f77ce2100eb0cc0bcded9d715dae
Tharun Kaarthik G K <128490140+G-K-T-K@users.noreply.github.com>
2024-12-04 09:41:41 +0000

    ..

--- Ceaser Cipher.py
+++ Ceaser Cipher.py
@@ -1,7 +1,6 @@
-# plain = 'a'
-# cipher = {
-#     "a" : "C",
-#     "b" : "D"
-# }
-# print(cipher["a"])
-
+user_input = input("enter a letter : ")
+k = int(input("constant : "))
+user_input = user_input.upper()
+ascii_value = ord(user_input)
+new_ascii_value = (ascii_value - 65 + k) % 26 + 65
+print(chr(new_ascii_value))
