ccc98e8117586c3a30d80fdb5fbe6b76c9c062e4
Tharun Kaarthik G K <128490140+G-K-T-K@users.noreply.github.com>
2025-03-04 13:17:19 +0000

    decryption

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -1,3 +1,5 @@
+#ENCRYPTION
+
 def generate_key(msg, key):
     key = list(key)
     if len(msg) == len(key):
@@ -27,3 +29,34 @@ key = input("Enter the key: ")
 
 encrypted_text = encrypt_vigenere(text_to_encrypt, key)
 print(f"Encrypted Text: {encrypted_text}")
+
+#DECRYPTION
+
+def generate_key(msg, key):
+    key = list(key)
+    if len(msg) == len(key):
+        return key
+    else:
+        for i in range(len(msg) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+def decrypt_vigenere(encrypted_text, key):
+    decrypted_text = []
+    key = generate_key(encrypted_text, key)
+    for i in range(len(encrypted_text)):
+        char = encrypted_text[i]
+        if char.isupper():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+        elif char.islower():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
+        else:
+            decrypted_char = char
+        decrypted_text.append(decrypted_char)
+    return "".join(decrypted_text)
+
+encrypted_text = input("Enter the encrypted text: ")
+key = input("Enter the key: ")
+
+decrypted_text = decrypt_vigenere(encrypted_text, key)
+print(f"Decrypted Text: {decrypted_text}")
