09d92b148905fec8126dcf4ac0fe66a4d78e34fb
Tharun Kaarthik G K <128490140+G-K-T-K@users.noreply.github.com>
2025-03-04 13:28:35 +0000

    ....

--- vernam cipher.py
+++ vernam cipher.py
@@ -1,44 +1,40 @@
+def generateKey(text, key):
+    key = list(key)
+    if len(text) == len(key):
+        return key
+    else:
+        for i in range(len(text) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
 def stringEncryption(text, key):
+    key = generateKey(text, key)
     cipherText = ""
-    cipher = []
 
-    for i in range(len(key)):
-        cipher.append(ord(text[i]) - ord('A') + ord(key[i]) - ord('A'))
-
-    for i in range(len(key)):
-        if cipher[i] > 25:
-            cipher[i] = cipher[i] - 26
-
-    for i in range(len(key)):
-        x = cipher[i] + ord('A')
-        cipherText += chr(x)
+    for i in range(len(text)):
+        shift = (ord(text[i]) - ord('A') + ord(key[i]) - ord('A')) % 26
+        cipherText += chr(shift + ord('A'))
 
     return cipherText
 
-def stringDecryption(s, key):
+def stringDecryption(cipherText, key):
+    key = generateKey(cipherText, key)
     plainText = ""
-    plain = []
 
-    for i in range(len(key)):
-        plain.append(ord(s[i]) - ord('A') - (ord(key[i]) - ord('A')))
-
-    for i in range(len(key)):
-        if plain[i] < 0:
-            plain[i] = plain[i] + 26
-
-    for i in range(len(key)):
-        x = plain[i] + ord('A')
-        plainText += chr(x)
+    for i in range(len(cipherText)):
+        shift = (ord(cipherText[i]) - ord('A') - (ord(key[i]) - ord('A'))) % 26
+        plainText += chr(shift + ord('A'))
 
     return plainText
 
-# Taking input for encryption
+# Taking input
 plainText = input("Enter the text: ").upper()
 key = input("Enter the key: ").upper()
 
+# Encryption
 encryptedText = stringEncryption(plainText, key)
-print("Cipher Text - " + encryptedText)
+print("Cipher Text -", encryptedText)
 
-# Taking input for decryption
+# Decryption
 decryptedText = stringDecryption(encryptedText, key)
-print("Decrypted Text - " + decryptedText)
+print("Decrypted Text -", decryptedText)
