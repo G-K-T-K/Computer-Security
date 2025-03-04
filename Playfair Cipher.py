821ea7b428cc54a6f3f5689ad806d67ed2a7b40b
Tharun Kaarthik G K <128490140+G-K-T-K@users.noreply.github.com>
2025-03-04 12:56:26 +0000

    decryption

--- Playfair Cipher.py
+++ Playfair Cipher.py
@@ -1,3 +1,5 @@
+#ENCRYPTION
+
 import string
 
 key = input("Enter the key: ").upper()
@@ -32,3 +34,36 @@ for a, b in message_pairs:
         encrypted_message.append(table[r2][c1])
 
 print("Encrypted:", ''.join(encrypted_message))
+
+#DECRYPTION
+
+import string
+
+key = input("Enter the key: ").upper()
+encrypted_message = input("Enter the encrypted message: ").upper()
+
+# Create Playfair Cipher table
+key = ''.join(sorted(set(key.replace("J", "I")), key=key.index))
+alphabet = string.ascii_uppercase.replace('J', '')
+table = key + ''.join([ch for ch in alphabet if ch not in key])
+table = [table[i:i+5] for i in range(0, len(table), 5)]
+
+# Split message into pairs
+message_pairs = [encrypted_message[i:i+2] for i in range(0, len(encrypted_message), 2)]
+
+decrypted_message = []
+for a, b in message_pairs:
+    r1, c1 = next((r, row.index(a)) for r, row in enumerate(table) if a in row)
+    r2, c2 = next((r, row.index(b)) for r, row in enumerate(table) if b in row)
+
+    if r1 == r2:
+        decrypted_message.append(table[r1][(c1 - 1) % 5])
+        decrypted_message.append(table[r2][(c2 - 1) % 5])
+    elif c1 == c2:
+        decrypted_message.append(table[(r1 - 1) % 5][c1])
+        decrypted_message.append(table[(r2 - 1) % 5][c2])
+    else:
+        decrypted_message.append(table[r1][c2])
+        decrypted_message.append(table[r2][c1])
+
+print("Decrypted:", ''.join(decrypted_message))
