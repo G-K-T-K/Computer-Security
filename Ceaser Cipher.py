ec48ce464d4b7c5093f6506217c79084b61316cf
Tharun Kaarthik G K <128490140+G-K-T-K@users.noreply.github.com>
2025-03-04 12:43:28 +0000

    decrption added

--- Ceaser Cipher.py
+++ Ceaser Cipher.py
@@ -1,3 +1,5 @@
+# ENCRYPTION
+
 user_input = input("Enter a word: ")
 k = int(input("Constant: "))
 user_input = user_input.upper()
@@ -9,3 +11,16 @@ for letter in user_input:
     encrypted_word += chr(new_ascii_value)
 
 print("Encrypted word:", encrypted_word)
+
+#DECRYPTION
+
+encrypted_word = input("Enter the encrypted word: ")
+k = int(input("Constant: "))
+decrypted_word = ""
+
+for letter in encrypted_word:
+    ascii_value = ord(letter)
+    new_ascii_value = (ascii_value - 65 - k) % 26 + 65
+    decrypted_word += chr(new_ascii_value)
+
+print("Decrypted word:", decrypted_word)
